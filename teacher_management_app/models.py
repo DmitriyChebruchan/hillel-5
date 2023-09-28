7a8d12aff45fb5e812977d0aca31830357be9ab2
DmitriyChebruchan <chebruchan2707@gmail.com>
2023-09-28 16:26:46 +0300

    __pycache__ is removed

--- teacher_management_app/models.py
+++ teacher_management_app/models.py
@@ -1,9 +1,12 @@
 from django.db import models
 
+
 # Create your models here.
+class Teacher(models.Model):
+    first_name = models.CharField(max_length=50)
+    surname = models.CharField(max_length=50)
+    age = models.IntegerField()
+    subject = models.CharField(max_length=100)
 
-
-
-
-def dummy_2():
-    return 2
+    def __str__(self):
+        return f"{self.first_name} {self.surname}"
--- teacher_management_app/views.py
+++ teacher_management_app/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+import random
+from faker import Faker
 
-# Create your views here.
+fake = Faker()
+subjects = ["Math", "English", "Ethics"]
+
+
+def teacher_generator():
+    first_name = fake.first_name()
+    surname = fake.surname()
+    age = random(21,87)
+    subject = random.choice(subjects)
