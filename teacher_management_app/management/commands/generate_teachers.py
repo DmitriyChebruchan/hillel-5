a76780a36e380b81e57d3fe94708788cb883b86d
DmitriyChebruchan <chebruchan2707@gmail.com>
2023-10-03 17:24:30 +0300

    black check performed

--- teacher_management_app/management/commands/generate_teachers.py
+++ teacher_management_app/management/commands/generate_teachers.py
@@ -26,7 +26,6 @@ class Command(BaseCommand):
 
             self.stdout.write(
                 self.style.SUCCESS(
-                    '{}. Successfully added teacher "{}"'.format(
-                        i + 1, t.first_name)
+                    '{}. Successfully added teacher "{}"'.format(i + 1, t.first_name)
                 )
             )
