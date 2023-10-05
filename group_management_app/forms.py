23b535931e5e9218dfc4e83478be2a30ab6aac2d
DmitriyChebruchan <chebruchan2707@gmail.com>
2023-10-05 09:54:26 +0300

    fixing mistakes is requirements.txt and commented code

--- group_management_app/forms.py
+++ group_management_app/forms.py
@@ -7,12 +7,3 @@ class GroupForm(forms.ModelForm):
     class Meta:
         model = Group
         fields = ["name", "curator"]
-
-    # def clean(self):
-    #     cleaned_data = super().clean()
-    #     name = cleaned_data.get('first_name')
-
-    #     # Check if 'first_name' contains digits
-    #     if any(char.isdigit() for char in name):
-    #         self.add_error(
-    #             'name', 'Name should not contain digits.')
